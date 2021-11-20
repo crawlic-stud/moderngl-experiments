88213a199089041d887c22f66a0a21679c40718e
crawlic-stud <nikitosik0726@gmail.com>
2021-11-20 18:20:19 +0300

    added new parametric functions

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # ModernGL python library experiments
-I'm not an author of this cardioid, original video: https://www.youtube.com/watch?v=iCuNaOyhMMY
+I'm not an author of this code, original video: https://www.youtube.com/watch?v=iCuNaOyhMMY
+
 ***
-![](pics/cardioid.gif)
+![](pics/infinity.gif)
 ***
-I'm going to use this repository for studying opengl and shaders
+
--- shaders/fragment_shader.glsl
+++ shaders/fragment_shader.glsl
@@ -21,27 +21,52 @@ void main() {
     vec2 uv = (gl_FragCoord.xy - 0.5 * resolution.xy) / resolution.y;
     vec3 col = vec3(0.0);
 
-    uv = rotate2D(uv, 3.14 / 2);
+    // uv = rotate2D(uv, 3.14 / 2);
 
     float r = 0.17;
-    for (float i=0.0; i < 60.0; i++) {
+    float max_i = 360.0;
+    int scale_factor = 10;
+    for (float i=0.0; i < max_i; i++) {
 
-        float factor = (sin(time) * 0.5 + 0.5) + 0.3;
+        float factor = ((sin(time) * 0.5 + 0.5) + 0.3) / 100;
         i += factor;
 
-        float a = i / 3;
-        float dx = 2 * r * cos(a) - r * cos(2 * a);
-        float dy = 2 * r * sin(a) - r * sin(2 * a);
+        float a = i / scale_factor;
+
+        // cardioid:
+        // float dx = 2 * r * cos(a) - r * cos(2 * a);
+        // float dy = 2 * r * sin(a) - r * sin(2 * a);
+
+        // spiral:
+        // float dx = a * sin(a) / 50;
+        // float dy = a * cos(a) / 50;
+
+        // some sort of star:
+        // float dx = 4.4 * (cos(a) + cos(1.1 * a) / 1.1) / 20;
+        // float dy = 4.4 * (sin(a) - sin(1.1 * a) / 1.1) / 20;
+
+        // another star:
+        // scale_factor = 20
+        // float dx = 24.8 * (cos(a) + cos(6.2 * a) / 6.2) / 70;
+        // float dy = 24.8 * (sin(a) - sin(6.2 * a) / 6.2) / 70;
+
+        // infinity sign:
+        float dx = sin(a + 3.14 / 2) / 1.5;
+        float dy = sin(2 * a) / 3;
+
+        // heart:
+        // float dx = 16 * pow(sin(a), 3.0) / 50;
+        // float dy = (13 * cos(a) - 5 * cos(2 * a) - 2 * cos(3 * a) - cos(4 * a)) / 50;
 
         // with shake:
-        // col += 0.005 * factor / length(uv - vec2(dx + 0.1, dy) - 0.02 * random(i));
+        // col += 0.005 * factor / length(uv - vec2(dx - 0.5, dy) - 0.02 * random(i));
 
         // without shake
-        col += 0.005 * factor / length(uv - vec2(dx + 0.1, dy));
+        // col += 0.0005 * factor / length(uv - vec2(dx + 0.1, dy));
+        col += 0.001 / length(uv - vec2(dx, dy));
     }
 
     // color change
     col *= sin(vec3(0.2, 0.8, 0.9) * time) * 0.15 + 0.25;
-
     fragColor = vec4(col, 1.0);
 }
